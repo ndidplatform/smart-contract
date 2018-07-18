68f367e762fb77c38cd7d9521da995e66d81a18e
Raphassit Leelachaikul <raphassitlee@gmail.com>
2018-07-18 16:52:40 +0700

    ..

--- abci/server.go
+++ abci/server.go
@@ -28,33 +28,33 @@ import (
 	"strings"
 
 	"github.com/ndidplatform/smart-contract/abci/did"
-	// "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 	server "github.com/tendermint/tendermint/abci/server"
 	"github.com/tendermint/tendermint/abci/types"
 	cmn "github.com/tendermint/tendermint/libs/common"
-	// tdmLog "github.com/tendermint/tendermint/libs/log"
+	tdmLog "github.com/tendermint/tendermint/libs/log"
 )
 
 type loggerWriter struct{}
 
-// var log *logrus.Entry
+var log *logrus.Entry
 
 func init() {
 	// Set default logrus
 	logFile, _ := os.OpenFile("DID.log", os.O_CREATE|os.O_WRONLY, 0666)
 	// TODO: add evironment for write log
 	// Set write log to file
-	// if false {
-	// 	logrus.SetOutput(logFile)
-	// } else {
-	// 	logrus.SetOutput(os.Stdout)
-	// }
-	// logrus.SetLevel(logrus.DebugLevel)
-	// customFormatter := new(logrus.TextFormatter)
-	// customFormatter.TimestampFormat = "2006-01-02 15:04:05"
-	// customFormatter.FullTimestamp = true
-	// logrus.SetFormatter(customFormatter)
-	// log = logrus.WithFields(logrus.Fields{"module": "abci-app"})
+	if false {
+		logrus.SetOutput(logFile)
+	} else {
+		logrus.SetOutput(os.Stdout)
+	}
+	logrus.SetLevel(logrus.DebugLevel)
+	customFormatter := new(logrus.TextFormatter)
+	customFormatter.TimestampFormat = "2006-01-02 15:04:05"
+	customFormatter.FullTimestamp = true
+	logrus.SetFormatter(customFormatter)
+	log = logrus.WithFields(logrus.Fields{"module": "abci-app"})
 }
 
 func main() {
@@ -67,15 +67,15 @@ func runABCIServer(args []string) {
 	var app types.Application
 	app = did.NewDIDApplication()
 
-	// writer := newLoggerWriter()
-	// logger := tdmLog.NewTMLogger(tdmLog.NewSyncWriter(writer))
+	writer := newLoggerWriter()
+	logger := tdmLog.NewTMLogger(tdmLog.NewSyncWriter(writer))
 
 	// Start the listener
 	srv, err := server.NewServer(address, "socket", app)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	// srv.SetLogger(logger.With("module", "abci-server"))
+	srv.SetLogger(logger.With("module", "abci-server"))
 	if err := srv.Start(); err != nil {
 		fmt.Println(err.Error())
 	}
@@ -86,35 +86,35 @@ func runABCIServer(args []string) {
 	})
 }
 
-// func newLoggerWriter() *loggerWriter {
-// 	return &loggerWriter{}
-// }
+func newLoggerWriter() *loggerWriter {
+	return &loggerWriter{}
+}
 
-// func (w *loggerWriter) Write(p []byte) (int, error) {
-// 	allMsg := strings.Fields(string(p))
-// 	charType := allMsg[0][0]
+func (w *loggerWriter) Write(p []byte) (int, error) {
+	allMsg := strings.Fields(string(p))
+	charType := allMsg[0][0]
 
-// 	keyValues := make(map[string]interface{})
-// 	newMsg := ""
+	keyValues := make(map[string]interface{})
+	newMsg := ""
 
-// 	for index, msg := range allMsg {
-// 		if index > 0 {
-// 			if strings.Contains(msg, "=") {
-// 				kv := strings.Split(msg, "=")
-// 				keyValues[kv[0]] = kv[1]
-// 			} else {
-// 				newMsg += msg + " "
-// 			}
-// 		}
-// 	}
+	for index, msg := range allMsg {
+		if index > 0 {
+			if strings.Contains(msg, "=") {
+				kv := strings.Split(msg, "=")
+				keyValues[kv[0]] = kv[1]
+			} else {
+				newMsg += msg + " "
+			}
+		}
+	}
 
-// 	switch string(charType) {
-// 	case "D":
-// 		log.WithFields(keyValues).Debug(newMsg)
-// 	case "E":
-// 		log.WithFields(keyValues).Error(newMsg)
-// 	default:
-// 		log.WithFields(keyValues).Info(newMsg)
-// 	}
-// 	return 0, nil
-// }
+	switch string(charType) {
+	case "D":
+		log.WithFields(keyValues).Debug(newMsg)
+	case "E":
+		log.WithFields(keyValues).Error(newMsg)
+	default:
+		log.WithFields(keyValues).Info(newMsg)
+	}
+	return 0, nil
+}
