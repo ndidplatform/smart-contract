b891177cb97066c142dbe6a4d6459279cb987e1d
Raphassit Leelachaikul <raphassitlee@gmail.com>
2018-07-20 11:03:51 +0700

    Use switch case instead of reflect to call a function

--- abci/did/query.go
+++ abci/did/query.go
@@ -23,8 +23,7 @@
 package did
 
 import (
-	"reflect"
-
+	"github.com/ndidplatform/smart-contract/abci/code"
 	"github.com/tendermint/tendermint/abci/types"
 )
 
@@ -40,41 +39,59 @@ func ReturnQuery(value []byte, log string, height int64, app *DIDApplication) ty
 
 // QueryRouter is Pointer to function
 func QueryRouter(method string, param string, app *DIDApplication, height int64) types.ResponseQuery {
-	funcs := map[string]interface{}{
-		"GetNodePublicKey":             getNodePublicKey,
-		"GetIdpNodes":                  getIdpNodes,
-		"GetRequest":                   getRequest,
-		"GetRequestDetail":             getRequestDetail,
-		"GetAsNodesByServiceId":        getAsNodesByServiceId,
-		"GetMsqAddress":                getMsqAddress,
-		"GetNodeToken":                 getNodeToken,
-		"GetPriceFunc":                 getPriceFunc,
-		"GetUsedTokenReport":           getUsedTokenReport,
-		"GetServiceDetail":             getServiceDetail,
-		"GetNamespaceList":             getNamespaceList,
-		"CheckExistingIdentity":        checkExistingIdentity,
-		"GetAccessorGroupID":           getAccessorGroupID,
-		"GetAccessorKey":               getAccessorKey,
-		"GetServiceList":               getServiceList,
-		"GetNodeMasterPublicKey":       getNodeMasterPublicKey,
-		"GetNodeInfo":                  getNodeInfo,
-		"CheckExistingAccessorID":      checkExistingAccessorID,
-		"CheckExistingAccessorGroupID": checkExistingAccessorGroupID,
-		"GetIdentityInfo":              getIdentityInfo,
-		"GetDataSignature":             getDataSignature,
-		"GetIdentityProof":             getIdentityProof,
-		"GetServicesByAsID":            getServicesByAsID,
-	}
-	value, _ := callQuery(funcs, method, param, app, height)
-	return value[0].Interface().(types.ResponseQuery)
+	result := callQuery(method, param, app, height)
+	return result
 }
 
-func callQuery(m map[string]interface{}, name string, param string, app *DIDApplication, height int64) (result []reflect.Value, err error) {
-	f := reflect.ValueOf(m[name])
-	in := make([]reflect.Value, 3)
-	in[0] = reflect.ValueOf(param)
-	in[1] = reflect.ValueOf(app)
-	in[2] = reflect.ValueOf(height)
-	result = f.Call(in)
-	return
+func callQuery(name string, param string, app *DIDApplication, height int64) types.ResponseQuery {
+	switch name {
+	case "GetNodePublicKey":
+		return getNodePublicKey(param, app, height)
+	case "GetIdpNodes":
+		return getIdpNodes(param, app, height)
+	case "GetRequest":
+		return getRequest(param, app, height)
+	case "GetRequestDetail":
+		return getRequestDetail(param, app, height)
+	case "GetAsNodesByServiceId":
+		return getAsNodesByServiceId(param, app, height)
+	case "GetMsqAddress":
+		return getMsqAddress(param, app, height)
+	case "GetNodeToken":
+		return getNodeToken(param, app, height)
+	case "GetPriceFunc":
+		return getPriceFunc(param, app, height)
+	case "GetUsedTokenReport":
+		return getUsedTokenReport(param, app, height)
+	case "GetServiceDetail":
+		return getServiceDetail(param, app, height)
+	case "GetNamespaceList":
+		return getNamespaceList(param, app, height)
+	case "CheckExistingIdentity":
+		return checkExistingIdentity(param, app, height)
+	case "GetAccessorGroupID":
+		return getAccessorGroupID(param, app, height)
+	case "GetAccessorKey":
+		return getAccessorKey(param, app, height)
+	case "GetServiceList":
+		return getServiceList(param, app, height)
+	case "GetNodeMasterPublicKey":
+		return getNodeMasterPublicKey(param, app, height)
+	case "GetNodeInfo":
+		return getNodeInfo(param, app, height)
+	case "CheckExistingAccessorID":
+		return checkExistingAccessorID(param, app, height)
+	case "CheckExistingAccessorGroupID":
+		return checkExistingAccessorGroupID(param, app, height)
+	case "GetIdentityInfo":
+		return getIdentityInfo(param, app, height)
+	case "GetDataSignature":
+		return getDataSignature(param, app, height)
+	case "GetIdentityProof":
+		return getIdentityProof(param, app, height)
+	case "GetServicesByAsID":
+		return getServicesByAsID(param, app, height)
+	default:
+		return types.ResponseQuery{Code: code.UnknownMethod, Log: "Unknown method"}
+	}
 }
