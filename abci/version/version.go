e11e6f1fc27e0cb0f6f6ff3138198120f4a92386
Raphassit Leelachaikul <raphassitlee@gmail.com>
2019-11-21 11:21:47 +0700

    Bump version

--- abci/version/version.go
+++ abci/version/version.go
@@ -41,7 +41,7 @@ func init() {
 
 const (
 	// ABCIAppSemVer is ABCI app version.
-	ABCIAppSemVer = "4.0.0"
+	ABCIAppSemVer = "4.1.0"
 
 	// ABCIAppProtocolVersion is ABCI App protocol version.
 	ABCIAppProtocolVersion = 2
